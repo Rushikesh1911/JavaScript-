cf3e7e29b2764134343c20f8519c00f6ed88c9d7
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-04 08:09:11 +0000

    javascript  data type and variables afternoon 1

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 2458871
+let accountEmail = "kulkarnirushikesh@gmail.com"
+var accountPassword = "19112006"
+accountCity = "Kalyan"
+ let accountState;
+// accountId = 2 // not allowed 
+accountEmail = "kulkarnirushi@gmail.com"
+accountPassword = "04012025"
+accountCity = "Dombivali"
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword , accountCity, accountState])

605cc507e6633b00d7e511bcdcf059c8076e0c86
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-11 08:01:27 +0000

    conversion of data type

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "hitesh";
+
+
+console.log(typeof score );
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+let  isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn );
+
+
+// covrting number "33"  => 33
+// "33abc" => NaN
+// 1 => true ; 0=> flase
+// "" => false  ; "Rushikwsh"  => true
+
+
+let somenumber = 33
+
+let stringNumber = String(somenumber);
+
+console.log(stringNumber);
+console.log( typeof stringNumber); //it seems to be a number but it is converted into string
+
+
+

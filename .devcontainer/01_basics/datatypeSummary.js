494188d52d8bb4b882ce90c180ddbb3dbc3b6489
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-11 18:05:31 +0000

    summary data type

---
+++ .devcontainer/01_basics/datatypeSummary.js
@@ -0,0 +1,45 @@
+// Primitive -->
+
+// 1.String
+// 2.Number
+// 3.Boolean
+// 4.null
+// 5.undefined
+// 6.Symbol
+// 7.BigInt 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 8850075312
+console.log(bigNumber);
+
+
+// Reference (Non Primitive )
+
+// 1.Array
+// 2.Objects
+// 3.Functions 
+
+
+
+
+
+
+
+const heros = ["Shaktiman " , "nagraj" , "doga"];
+let myobj = {
+    name : "Rushikesh",
+    age : 22,
+
+
+}
+ const myfunction = function(){
+ console.log("Hello World");
+
+ 
+ }
+
+ console.log(typeof anotherId);
+ 

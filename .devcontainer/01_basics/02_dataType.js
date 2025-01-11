a435f27a728102d1189103b7fc8fe984a4605751
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-11 07:34:40 +0000

    02_datatype

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3 ) //using node.js not browser 
+
+console.log(3 + 3);
+
+
+
+
+let name = "Rushikesh";
+let age = 18;
+let isLogin = false;
+let state;
+
+
+// number =>> 2 to the power 53
+// bigint
+// string  =>> ""
+// boolean =>> True /False
+// null =>> standalone value 
+// undefined =>> 
+// symbol =>> unique
+
+
+// object
+
+console.log(typeof undefined); //undefined type 
+console.log(typeof null);  // object type 
+

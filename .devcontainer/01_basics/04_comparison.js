a5df78ef0dbe16e7638bdd100e3c7caa361f822a
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-11 17:44:00 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+console.log(2>1);
+
+
+
+
+console.log("2" > 1);
+console.log("02" > 1 );
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);  // here it converts null into 0
+
+console.log(undefined==0);
+
+console.log(undefined>=0);
+
+
+console.log(undefined>0);
+
+
+// === checks value and datatype both
+
+console.log("2"==2);
+
+
+
+
+//summary => 
+    //upper these create the confusion in output ....

22f887ebf3fcbf3e184f7048ffcc4e769439fc64
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-11 08:28:19 +0000

    datatype

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,17 +1,17 @@
-let score = "hitesh";
+// let score = "hitesh";
 
 
-console.log(typeof score );
+// console.log(typeof score );
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
-let  isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log( booleanIsLoggedIn );
+// let  isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn );
 
 
 // covrting number "33"  => 33
@@ -20,12 +20,49 @@ console.log( booleanIsLoggedIn );
 // "" => false  ; "Rushikwsh"  => true
 
 
-let somenumber = 33
+// let somenumber = 33
 
-let stringNumber = String(somenumber);
+// let stringNumber = String(somenumber);
 
-console.log(stringNumber);
-console.log( typeof stringNumber); //it seems to be a number but it is converted into string
+// console.log(stringNumber);
+// console.log( typeof stringNumber); //it seems to be a number but it is converted into string
+
+
+
+
+//********************operations ****************************** */
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// let str1 = "Rushikesh"
+// let str2 = " Kulkarni"
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+//  console.log("1" + 2  + 2);  // here as string is first then it will consider all the remaining as string only 
+// console.log( 2 + 2 + "1");   // here the string is not first so then the elents will perform operations the join the string
+
+// console.log(+true);
+
+// let num1 , num2 ,num3 
+
+// num1 = num2 = num3
+
+let gameCounter = 300
+++gameCounter;
+console.log(gameCounter);
+
+//go and study the postfix and prefix  from js and mdn *** important 
+
+
+
+
 
 
 

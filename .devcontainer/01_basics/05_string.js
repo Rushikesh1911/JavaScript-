49e09739f6f6773f69c7d6777932ded13c12e7f3
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-12 17:35:14 +0000

    string basic

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,44 @@
+const name = "Rushikesh";
+const repoCount = 50;
+
+console.log('Hello my name is ${name}  and my repo count is ${repoCount}');
+
+
+const gameName = new String('rushikesh')
+
+
+
+ console.log(gameName[0]);
+
+ console.log(gameName.__proto__); 
+
+
+ console.log(gameName.length);
+ console.log(gameName.toLocaleUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('i'));
+
+
+
+
+
+
+
+ const newString = gameName.substring(0,4);
+console.log(newString)
+
+
+
+
+
+
+ const anotherString =gameName.slice(-8,4)
+ console.log(anotherString);
+
+
+
+
+
+
+const newStringOne = "     rushikesh      "   //sonmetimes when extra space or extra is needed to be cut trim is used
+console.log(newStringOne.trim());

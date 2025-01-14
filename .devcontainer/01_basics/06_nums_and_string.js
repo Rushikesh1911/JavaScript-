013c3bd221dd6e947d6f142a2c48aa1ed22fde8b
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-14 19:06:05 +0000

    mathAndNum

---
+++ .devcontainer/01_basics/06_nums_and_string.js
@@ -0,0 +1,43 @@
+const score = 400
+//console.log(score);
+
+
+const balance  =new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 19.1106
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000000000000000000000000000000
+//console.log(hundreds.toLocaleString('en-IND'));
+
+
+
+
+
+
+
+
+
+//*************************MATHS**************************** */
+
+
+
+
+// console.log(Math.abs(-897));     // absolute kar dega matlab agar negative ho to positive but viceversa nahi
+// console.log(Math.floor(5.67));   //integer ki value 
+// //console.log(Math.ciel(8.5));   // badadega value
+// console.log(Math.max(8,9,7));    // finds maximum value
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+
+const min = 19
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min)

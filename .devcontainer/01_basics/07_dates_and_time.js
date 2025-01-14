daeb14a22adc4754aa067de159061cf713f12d50
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-14 19:25:04 +0000

    almost date and time

---
+++ .devcontainer/01_basics/07_dates_and_time.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+// console.log(myDate); 
+// console.log(myDate.toString()); 
+//  console.log(myDate.toDateString()); 
+//  console.log(myDate.toLocaleString()); 
+
+
+ //let myCreatedDate = new Date("2025-01-15")
+ let myCreatedDate = new Date("01-14-2025")
+ console.log(myCreatedDate.toLocaleString());
+ console.log(myCreatedDate.toDateString());
+ 
+
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+
+ 
+ 

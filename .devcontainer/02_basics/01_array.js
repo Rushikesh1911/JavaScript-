9b47516b627b9a0eb6f68f1f9120c3e7a3523add
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-17 19:01:29 +0000

    array1

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,39 @@
+const arr = [1,9,1,1,2,0,0,6]
+// console.log(arr[3]);
+const myrole = ["Krishna" , "mahakal"]
+const arr1 = Array(1,2,3,4)
+// console.log(arr[2]);
+
+//array methods//
+
+arr.push(5)  // add or insert the element from the array
+arr.pop()  // deletes or remove the last elemnts from array
+arr.unshift(5) // add element in the starting or fist in the array
+arr.shift()  //remove element in the starting or fist in the array{ same as push and pop}
+
+// console.log(arr.includes(1)) // get to know whether element are present or not
+// console.log(arr.indexOf(9));  // get to know whether the element at given index
+const newarr1 =  arr.join()     // join adds the given arr into string
+
+
+//  console.log(arr);
+// console.log(newarr1);
+
+
+// slice  , spice
+
+// console.log("A", arr);
+
+const myn1 = arr.slice(1,3)   // does not manipulate original array
+// console.log(myn1);
+// console.log("B", arr);
+
+const myn2 = arr.splice(1,3)  // manipulates the original array
+
+// console.log("c", arr);
+// console.log(myn1);
+
+
+
+
+

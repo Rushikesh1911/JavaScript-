11260110d6aea388857dcee84fe261a139562309
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-17 17:57:50 +0000

    date and time modified

--- .devcontainer/01_basics/07_dates_and_time.js
+++ .devcontainer/01_basics/07_dates_and_time.js
@@ -7,18 +7,32 @@ let myDate = new Date()
 
  //let myCreatedDate = new Date("2025-01-15")
  let myCreatedDate = new Date("01-14-2025")
- console.log(myCreatedDate.toLocaleString());
- console.log(myCreatedDate.toDateString());
+//  console.log(myCreatedDate.toLocaleString());
+//  console.log(myCreatedDate.toDateString());
  
 
 
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
-console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000));
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+// '${newDate.getDay() and the time }'
+
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+})
+
 
 
 

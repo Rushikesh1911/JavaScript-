4f7b643ee58077e682a3b896883dc35b74c78e33
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-19 16:09:38 +0000

    object

---
+++ .devcontainer/02_basics/05_object.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename : "learning sanskrit",
+    courseInstructor: "Guruji",
+    price : "5100",
+}
+
+const{courseInstructor: instructor} = course      //destructuring of object 
+// console.log(instructor);
+
+
+
+//API            // apna kam durse ke upar
+
+// {
+//    "name": rushikesh
+//     "coursename": "learning vedas"
+//     "price": "free"
+// }
+
+[
+
+    {},
+    {},
+]

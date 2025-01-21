999f24eb24e3672acf6c7f5829e4cb431c02a725
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-01-21 17:01:48 +0000

    scope

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+
+
+let a = 842
+if(true){
+  
+let a = 10
+const b = 20
+console.log("INNER : " ,a);
+
+
+}
+
+
+ console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+for (let i = 0; i<Array.length; i++ ){
+  const element = Array[i]; 
+}
+
+

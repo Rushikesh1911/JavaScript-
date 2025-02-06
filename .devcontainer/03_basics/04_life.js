8a15ad520eb03d3953b17d1c05ebd27222b155c0
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-06 11:58:13 +0000

    IIFE

---
+++ .devcontainer/03_basics/04_life.js
@@ -0,0 +1,12 @@
+// immediately invoked function expression //  {IIFE}
+
+(function user(){
+    console.log(`db connected`);
+    
+})();
+
+((name)=>{
+    console.log(`db connecte ${name}`);
+    
+} )('rushikesh')
+

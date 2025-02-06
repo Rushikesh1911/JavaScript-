06d9ffc5e41d255e0a82be6ea741a0a9e3bd1384
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-06 11:47:48 +0000

    arrow more info

--- .devcontainer/03_basics/03_arrow.js
+++ .devcontainer/03_basics/03_arrow.js
@@ -1,11 +1,41 @@
-const user = {
-    username: "rushikesh",
-    price: 999,
+// const user = {
+//     username: "rushikesh",
+//     price: 999,
     
-    welcomeMessage: function() {
-        console.log(${this.username}, welcome to website);
-    }
-};
+//     welcomeMessage: function() {
+//         console.log(`${this.username}, welcome to website`);
+//     }
+// };
 
-user.welcomeMessage();  // Corrected method call
-console.log(this);  // Logs the global object, in a browser it will be window
+// user.welcomeMessage();  // Corrected method call
+// user.username = "sam"
+// console.log(this);  // Logs the global object, in a browser it will be window
+
+
+// function rushi(){
+//     let username = "rushikesh"
+//    console.log(this);  // function ke andr agar this lo print karte hai toh kuch toh atta hai
+// }
+// rushi()
+
+
+// const rushi = () => {
+//     let username = "rushikesh"
+//     console.log(this);
+// }
+
+// rushi()
+
+
+
+// const addTwo = (num1 , num2) =>   num1 + num2;    // implicit return type
+const addTwo = (num1,num2) => ( num1+num2)      // no need to use the return when parathesis are used()
+
+
+console.log(addTwo(5,7))
+
+const myArray = [2,8,5]
+    myArray.forEach()
+
+
+

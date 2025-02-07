9a182f1ad8bb5c20585cb8d12f7dcd640362b86a
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-07 18:34:59 +0000

    changed in last

--- .devcontainer/05_iterations_loops/eight.js
+++ .devcontainer/05_iterations_loops/eight.js
@@ -1,32 +1,32 @@
-const nums  = [1,2,3,4,5,6]
+// const nums  = [1,2,3,4,5,6]
 
-// const myTotal = nums.reduce(function (acc , curval){
-//     console.log(`acc:${acc} and currval :${curval}`)
-//     return acc + curval
-// },0)
+// // const myTotal = nums.reduce(function (acc , curval){
+// //     console.log(`acc:${acc} and currval :${curval}`)
+// //     return acc + curval
+// // },0)
 
-// const myTotal = nums.reduce( (acc,curval)=> {
-//     return  acc + curval
-// },0)
+// // const myTotal = nums.reduce( (acc,curval)=> {
+// //     return  acc + curval
+// // },0)
 
-//console.log(myTotal);
+// //console.log(myTotal);
 
 
-const shoppingcart = [
-    {
-    itemName : "jsCourse",
-    price: 2999
-    },
-    {
-        itemName : "pyCourse",
-        price: 1999
-    },
-    {
-            itemName : "C++ course",
-            price: 4299
-    }
+// const shoppingcart = [
+//     {
+//     itemName : "jsCourse",
+//     price: 2999
+//     },
+//     {
+//         itemName : "pyCourse",
+//         price: 1999
+//     },
+//     {
+//             itemName : "C++ course",
+//             price: 4299
+//     }
 
-]
+// ]
 
- const PriceToPay = shoppingcart.reduce( (acc,item) => acc + item.price, 0)
-console.log(PriceToPay);
+//  const PriceToPay = shoppingcart.reduce( (acc,item) => acc + item.price, 0)
+// console.log(PriceToPay);

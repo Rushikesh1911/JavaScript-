32ed86e026f13cb3b03d144c2f6d3a1bd3f3a5ab
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-07 07:58:51 +0000

    wait

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,21 @@
+// // if 
+// const is_user_logged_in = true
+//  const temp = 37                                                               // >= , >= , > , < , = , == , ===  , != , !==
+// if(temp > 40){
+//     console.log("it is hot")
+// }
+// else {
+//     console.log("it is not so hot")
+// }
+
+
+// const score = 54;
+
+// if (score>50){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+// console.log(`User power : ${power}`)
+
+
+

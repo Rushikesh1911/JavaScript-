b2532dcad7c48e52197ed797ef445c58d709ba3b
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-07 18:34:23 +0000

    filter map and reduce

---
+++ .devcontainer/05_iterations_loops/six.js
@@ -0,0 +1,29 @@
+// const coding = ["js" , "ruby" , "python"]
+
+// const values = coding.forEach ((item) =>{
+//     //console.log(item);
+//     return item;
+    
+// })
+// console.log(values);
+
+
+ const num = [1,2,3,4,5,6,7,8,9,10]
+
+//  const newNum = num.filter( (num1) => {
+//     return num1>4
+//  } )  // basics of filter
+// console.log(newNum);
+
+
+
+// const newNums = []
+// num.forEach( (num1) => {
+    // if(num1>4){                                    // same as above as filter just this is by using forEach loop 
+//         newNums.push(num1)
+//     }
+// })
+//     console.log(newNums); 
+
+
+    

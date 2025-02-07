265de52597d874b5c8ec4ee5d8ee16dd6c491021
Rushikesh1911 <kulkarnirushikesh905@gmail.com>
2025-02-07 16:54:43 +0000

    while_doWhile_loop

---
+++ .devcontainer/05_iterations_loops/two.js
@@ -0,0 +1,27 @@
+
+let index = 0
+while(index<=10){
+    //console.log(`Value of index is ${index}`);
+    index = index +2
+}
+
+let arr  = 0
+while(arr< arr.length){
+    //console.log(`Value is ${arr[arr]}`);
+    arr++;
+    
+}
+ let runs = 1;
+
+do {
+    //console.log(`Runs is ${runs}`);
+    runs++;
+} while (runs<=15);
+
+ let score = 11;
+
+do {
+    console.log(`score is ${score}`);
+    runs++;
+} while (score<=10);
+

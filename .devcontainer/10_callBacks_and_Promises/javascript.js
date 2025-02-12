8b99804529cbdd781d4b82d45b01ebc0e2a277c3
Rushikesh Sudhir Kulkarni <kulkarnirushikesh905@gmail.com>
2025-02-12 16:25:32 +0000

    I made changes after learning javascript

---
+++ .devcontainer/10_callBacks_and_Promises/javascript.js
@@ -0,0 +1,34 @@
+console.log("rushi is good");
+console.log("rushikesh is good");
+
+
+
+setTimeout(() => {
+    console.log("I am inside settimeout");
+    
+},3500);
+
+setTimeout(() => {
+    console.log("I am doing js");
+    
+},4850);
+console.log("the end");
+
+const fn = ()=>{
+    console.log("nothing");
+    
+}
+
+const callback = (arg)=>{
+    console.log(arg);
+    fn()
+    
+}
+const loadscript = (src,callback) => {
+   let sc =  document.createElement("script");
+  sc.src =src;
+  sc.onload  =  callback("rushi" ,fn);
+  document.head.append(sc)
+}
+
+loadscript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js",callback)
---
+++ .devcontainer/10_callBacks_and_Promises/promise.js
@@ -0,0 +1,39 @@
+console.log('rushi is good');
+
+let prom1 = new Promise((resolve , reject)=>{
+    let a  = Math.random();
+    if(a<0.5){
+        reject("no random number was not supporting you")
+    }
+    else{
+        setTimeout(()=>{
+            console.log("yes i am done ")
+            resolve("happy")   
+        },3000);
+    }
+})
+
+
+let prom2 = new Promise((resolve , reject)=>{
+    let a  = Math.random();
+    if(a<0.5){
+        reject("no random number was not supporting you")
+    }
+    else{
+        setTimeout(()=>{
+            console.log("yes i am done 2")
+            resolve("happy 2")   
+        },3000);
+    }
+})
+    
+
+let p3 = Promise.all([prom1, prom2])
+p3.then(()=>{
+    console.log(a);
+    
+}).catch(err=>{
+    console.log(err);
+    
+})
+

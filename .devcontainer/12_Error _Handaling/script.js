218d4315f7bbef5a7b6a6b75f0089b085b05cbc9
Rushikesh Kulkarni <kulkarnirushikesh905@gmail.com>
2025-02-17 23:56:20 +0530

    started the error handaling

---
+++ .devcontainer/12_Error _Handaling/script.js
@@ -0,0 +1,6 @@
+let a =  prompt("enter a first number")
+
+let b  = prompt("Enter the second number")
+
+let sum =  a+b                     // this gives error to this
+console.log("the sum is " , sum);

a5dc0a1e0195ae7ad9f483e09bfa74e6e04d8736
Rushikesh Kulkarni <kulkarnirushikesh905@gmail.com>
2025-02-18 23:04:56 +0530

    await and promise

--- .devcontainer/11_Async_Await_Fetch/await_&fetchApi/script.js
+++ .devcontainer/11_Async_Await_Fetch/await_&fetchApi/script.js
@@ -1,33 +1,58 @@
-function asyncFunc1() {
-    return new Promise((resolve, reject) => {
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data1");
+//             resolve("success");
+//         }, 3500);
+//     });
+// }
+
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data2");
+//             resolve("success");
+//         }, 6000);
+//     });
+// }
+
+// console.log("fetching data1...");
+// promise chain 
+
+
+function getData(dataId)
+{
+    return new Promise((resolve ,reject) =>{
         setTimeout(() => {
-            console.log("data1");
+            console.log("data", dataId);
             resolve("success");
-        }, 3500);
-    });
+            
+        },3000);
+    })
 }
-
-function asyncFunc2() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data2");
-            resolve("success");
-        }, 6000);
-    });
-}
-
-console.log("fetching data1...");
-
-let p1 = asyncFunc1();
-p1.then((res) => {
+getData(1).then((res) =>{
     console.log(res);
-    console.log("fetching data2..."); // then nesting like if I fetch data 1 then only fetch data 2
-
-    let p2 = asyncFunc2();
-    p2.then((res) => {
+    return getData(2).then(()=>{
         console.log(res);
-    });
-});
+        
+    })
+    
+}).then
+
+
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//     console.log(res);
+//     console.log("fetching data2..."); // then nesting like if I fetch data 1 then only fetch data 2
+
+//     let p2 = asyncFunc2();
+//     p2.then((res) => {
+//         console.log(res);
+//     });
+// });
+
+
+
 
 // console.log("fetching data2...");
 
